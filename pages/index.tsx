f54d0f08f9c258cd56f7e73e1471cdfc812b0177
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-08-22 18:03:01 -0300

    Home, hay que solucionar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,28 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { useEffect, useState } from 'react'
 import Navbar from '@components/NavBar/Navbar'
 import Header from '@components/Header/Header'
+import ProductList from '@components/ProductList/ProductList'
+
 
 const Home: NextPage = () => {
+  const [products, setProducts] = useState<TProduct[]>([])
+  useEffect(() => {
+    window
+      .fetch('/api/allPaltas')
+      .then((res) => res.json())
+      .then(({ data }: TAPIAvoResponse) => setProducts(data))
+  })
   return (
     <div className=''>
       <Head>
         <title>Home</title>
       </Head>
 
-     <Navbar />
-     <Header />
+      <Navbar />
+      <Header />
+      <ProductList products={products} />
     </div>
   )
 }
